870d8792a6f0a0b5a4a9fd9fb8e7fada38a6331b
caporatang <caporatang@naver.com>
2024-06-16 22:05:14 +0900

    Docs : CHAR_VS_VARCHAR.md

---
+++ src/test/java/com/example/realmysql/RealMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
